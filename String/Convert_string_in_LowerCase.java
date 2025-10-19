c6cd59aadf6913e5a662b788ba2133194c4fe025
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-10-19 10:57:15 +0530

    Add files via upload

---
+++ String/Convert_string_in_LowerCase.java
@@ -0,0 +1,10 @@
+public class Convert_string_in_LowerCase {
+    public static void main(String[] args) {
+        
+
+        String s1 = "OMKAR MANE";
+        
+
+        System.out.println(s1.toLowerCase());
+    }
+}
---
+++ String/Convert_string_in_upperCase.java
@@ -0,0 +1,13 @@
+public class Convert_string_in_upperCase {
+    
+
+    public static void main(String[] args) {
+        
+
+        String s1 = "omkar mane";
+
+
+
+        System.out.println( s1.toUpperCase());
+    }
+}
